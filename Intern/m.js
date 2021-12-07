34bfc5aba36e388390404f292371453e26a44a6e
MadhanReddy1687 <87870529+MadhanReddy1687@users.noreply.github.com>
2021-12-07 14:47:50 +0530

    Add files via upload

---
+++ Intern/m.js
@@ -0,0 +1,9 @@
+function onHospital(){
+    document.getElementById("s2").style.display="block";
+}
+function onLogin(){
+    document.getElementById("s3").style.display="block";
+}
+function onBlood(){
+     document.getElementById("s4").style.display="block";
+}
